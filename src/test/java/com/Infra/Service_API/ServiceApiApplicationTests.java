8e3af93abb1b3c9a4b4342648db759d580d87b28
김원경 <kyg279044@gmail.com>
2024-07-13 21:22:37 +0900

    feat: 스프링부트 세팅

---
+++ src/test/java/com/Infra/Service_API/ServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Infra.Service_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
