cbdec7c4770d79b4b040451fcd42d71ef9c2b1cc
김원경 <kyg279044@gmail.com>
2024-07-14 03:11:07 +0900

    feat: 학생 서비스 로직

--- src/main/java/com/Infra/Service_API/domain/student/dto/StudentResponse.java
+++ src/main/java/com/Infra/Service_API/domain/student/dto/StudentResponse.java
@@ -12,13 +12,13 @@ public record StudentResponse (
         @Schema(description = "닉네임", example = "nickname", requiredMode = NOT_REQUIRED)
         String nickname,
 
-        @Schema(description = "이름", example = "최준호", requiredMode = NOT_REQUIRED)
+        @Schema(description = "이름", example = "홍길동", requiredMode = NOT_REQUIRED)
         String name,
 
         @Schema(description = "휴대폰 번호", example = "010-0000-0000", requiredMode = NOT_REQUIRED)
         String phoneNumber,
 
-        @Schema(description = "이메일 주소", example = "koin123@koreatech.ac.kr", requiredMode = NOT_REQUIRED)
+        @Schema(description = "이메일 주소", example = "koin123@gmail.com", requiredMode = NOT_REQUIRED)
         String email,
 
         @Schema(description = "성별(남:0, 여:1)", example = "1", requiredMode = NOT_REQUIRED)
